3b49d01121dca0128c3cd7101df2de6f92dbf1fb
samarkun <samarkun4@gmail.com>
2025-03-25 21:04:22 -0700

    Database file and function created to connect database

---
+++ Backend/db/db.js
@@ -0,0 +1,13 @@
+import mongooes from 'mongoose'
+import { DB_NAME } from '../Dbname.js';
+
+const connectToDb = async() => {
+    try {
+        const connection = await mongooes.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n Mongo connected !! DB Host: ${connection.connection.host}`)
+    } catch (error) {
+        console.log("MONGODB connection error", error);
+        process.exit(1);
+    }
+}
+export default connectToDb
