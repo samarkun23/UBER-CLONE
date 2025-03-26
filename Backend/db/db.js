ae346d9631f4a227926a846a7df2e8cff5f1ef12
samarkun <samarkun4@gmail.com>
2025-03-25 21:08:59 -0700

    fixing spelling

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,9 +1,9 @@
-import mongooes from 'mongoose'
+import mongoose from 'mongoose'
 import { DB_NAME } from '../Dbname.js';
 
 const connectToDb = async() => {
     try {
-        const connection = await mongooes.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        const connection = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n Mongo connected !! DB Host: ${connection.connection.host}`)
     } catch (error) {
         console.log("MONGODB connection error", error);
