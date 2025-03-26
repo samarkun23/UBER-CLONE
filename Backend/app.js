bc069310f9cad2301ab6b2ef5ee18ff66c364854
samarkun <samarkun4@gmail.com>
2025-03-25 20:17:30 -0700

    Set up the basic server

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+dotenv.config();
+console.log()  //this is step ko deklena
+
+import express from 'express'
+import cors from 'cors'
+
+const app = express();
+
+app.use(cors());
+
+
+
+app.get('/', (req,res) => {
+    res.send('Hello world');
+});
+
+export default app;
